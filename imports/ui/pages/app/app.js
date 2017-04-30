51c2eef72f9aa840009891ba990b647c4bab5640
Mateus Rocha <mateus.mazzari@usp.br>
2017-04-29 22:22:59 -0300

    screen darkness when maximized

--- imports/ui/pages/app/app.js
+++ imports/ui/pages/app/app.js
@@ -6,3 +6,9 @@ import '../../components/nav/nav.js';
 Template.App_app.onCreated(function(){
   this.navState = new ReactiveVar("minimized");
 });
+
+Template.App_app.helpers({
+  dark(){
+    return (Template.instance().navState.get() === "maximized");
+  },
+});
